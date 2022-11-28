4534245026b6ded74a279bbb6bfc122523b464fb
Milena Kisyova <milena_kissyova@abv.bg>
2022-11-28 22:48:36 +0200

    Initial upload of my Bookstore app (used template).

---
+++ BookstoreApp/BookstoreApp.Common/GlobalConstants.cs
@@ -0,0 +1,9 @@
+﻿namespace BookstoreApp.Common
+{
+    public static class GlobalConstants
+    {
+        public const string SystemName = "BookstoreApp";
+
+        public const string AdministratorRoleName = "Administrator";
+    }
+}
---
+++ BookstoreApp/Services/BookstoreApp.Services.Data/SettingsService.cs
@@ -0,0 +1,29 @@
+﻿namespace BookstoreApp.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    using BookstoreApp.Data.Common.Repositories;
+    using BookstoreApp.Data.Models;
+    using BookstoreApp.Services.Mapping;
+
+    public class SettingsService : ISettingsService
+    {
+        private readonly IDeletableEntityRepository<Setting> settingsRepository;
+
+        public SettingsService(IDeletableEntityRepository<Setting> settingsRepository)
+        {
+            this.settingsRepository = settingsRepository;
+        }
+
+        public int GetCount()
+        {
+            return this.settingsRepository.AllAsNoTracking().Count();
+        }
+
+        public IEnumerable<T> GetAll<T>()
+        {
+            return this.settingsRepository.All().To<T>().ToList();
+        }
+    }
+}
