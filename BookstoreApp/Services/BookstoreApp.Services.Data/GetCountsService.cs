6dc1c4f4f6692bbe0cbb42a38cd282654901b386
Milena Kisyova <milena_kissyova@abv.bg>
2022-12-02 14:00:39 +0200

    I added GetCountsService.

---
+++ BookstoreApp/Services/BookstoreApp.Services.Data/GetCountsService.cs
@@ -0,0 +1,37 @@
+﻿namespace BookstoreApp.Services.Data
+{
+    using System.Linq;
+
+    using BookstoreApp.Data.Common.Repositories;
+    using BookstoreApp.Data.Models;
+    using BookstoreApp.Services.Data.Models;
+
+    public class GetCountsService : IGetCountsService
+    {
+        private readonly IDeletableEntityRepository<Book> booksRepository;
+        private readonly IDeletableEntityRepository<Author> authorsRepository;
+        private readonly IRepository<Genre> genresRepository;
+
+        public GetCountsService(
+            IDeletableEntityRepository<Book> booksRepository,
+            IDeletableEntityRepository<Author> authorsRepository,
+            IRepository<Genre> genresRepository)
+        {
+            this.booksRepository = booksRepository;
+            this.authorsRepository = authorsRepository;
+            this.genresRepository = genresRepository;
+        }
+
+        public CountsDto GetCounts()
+        {
+            var data = new CountsDto
+            {
+                BooksCount = this.booksRepository.All().Count(),
+                AuthorsCount = this.authorsRepository.All().Count(),
+                GenresCount = this.genresRepository.All().Count(),
+            };
+
+            return data;
+        }
+    }
+}
---
+++ BookstoreApp/Services/BookstoreApp.Services.Data/IGetCountsService.cs
@@ -0,0 +1,9 @@
+﻿using BookstoreApp.Services.Data.Models;
+
+namespace BookstoreApp.Services.Data
+{
+    public interface IGetCountsService
+    {
+        CountsDto GetCounts();
+    }
+}
