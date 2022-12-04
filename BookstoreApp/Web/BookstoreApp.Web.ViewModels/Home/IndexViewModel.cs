2094ee12237dec573945fd2b39a3d27ea174cea2
Milena Kisyova <milena_kissyova@abv.bg>
2022-12-04 19:06:17 +0200

    Added IndexViewModel in BookstoreApp.

---
+++ BookstoreApp/Web/BookstoreApp.Web.ViewModels/Home/IndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookstoreApp.Web.ViewModels.Home
+{
+    public class IndexViewModel
+    {
+        public int BooksCount { get; set; }
+
+        public int AuthorsCount { get; set; }
+
+        public int GenresCount { get; set; }
+    }
+}
