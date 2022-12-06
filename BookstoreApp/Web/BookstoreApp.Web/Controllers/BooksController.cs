2f27cd852a093ecb13c1362e85205b3b04569c73
Milena Kisyova <milena_kissyova@abv.bg>
2022-12-06 19:16:34 +0200

    I added BooksController and CategoriesController in BookstoreApp.

---
+++ BookstoreApp/Web/BookstoreApp.Web/Controllers/BooksController.cs
@@ -0,0 +1,17 @@
+﻿namespace BookstoreApp.Web.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BooksController : BaseController
+    {
+        public IActionResult Bestsellers()
+        {
+            return this.View();
+        }
+
+        public IActionResult NewBooks()
+        {
+            return this.View();
+        }
+    }
+}
---
+++ BookstoreApp/Web/BookstoreApp.Web/Controllers/CategoriesController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookstoreApp.Web.Controllers
+{
+    public class CategoriesController : BaseController
+    {
+        public IActionResult All()
+        {
+            return this.View();
+        }
+    }
+}
