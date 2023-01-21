5b772f9494698330a2fa871b1d4cda8f0a46394b
Milena Kisyova <milena_kissyova@abv.bg>
2023-01-21 20:13:28 +0200

    Made changes in BookstoreApp.

--- BookstoreApp/Web/BookstoreApp.Web/Controllers/AudioBooksController.cs
+++ BookstoreApp/Web/BookstoreApp.Web/Controllers/AudioBooksController.cs
@@ -1,6 +1,12 @@
 ﻿namespace BookstoreApp.Web.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     public class AudioBooksController : BaseController
     {
+        public IActionResult All()
+        {
+            return this.View();
+        }
     }
 }
--- BookstoreApp/Web/BookstoreApp.Web/Controllers/EBooksController.cs
+++ BookstoreApp/Web/BookstoreApp.Web/Controllers/EBooksController.cs
@@ -1,6 +1,12 @@
 ﻿namespace BookstoreApp.Web.Controllers
 {
+    using Microsoft.AspNetCore.Mvc;
+
     public class EBooksController : BaseController
     {
+        public IActionResult All()
+        {
+            return this.View();
+        }
     }
 }
