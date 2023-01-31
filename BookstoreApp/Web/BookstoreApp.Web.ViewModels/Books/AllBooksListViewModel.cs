2ad41753a7c2d55f5c6a603073b2d636a0c6a29d
Milena Kisyova <milena_kissyova@abv.bg>
2023-01-31 22:49:55 +0200

    Added AllBooksViewModel.cs and AllBooksListViewModel.cs.

---
+++ BookstoreApp/Web/BookstoreApp.Web.ViewModels/Books/AllBooksListViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BookstoreApp.Web.ViewModels.Books
+{
+    using System.Collections.Generic;
+
+    public class AllBooksListViewModel
+    {
+        public IEnumerable<AllBooksViewModel> Books { get; set; }
+
+        public int PageNumber { get; set; }
+    }
+}
---
+++ BookstoreApp/Web/BookstoreApp.Web.ViewModels/Books/AllBooksViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace BookstoreApp.Web.ViewModels.Books
+{
+    public class AllBooksViewModel
+    {
+        public int Id { get; set; }
+
+        public string ImageUrl { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public int AuthorId { get; set; }
+
+        public string Author { get; set; }
+
+    }
+}
