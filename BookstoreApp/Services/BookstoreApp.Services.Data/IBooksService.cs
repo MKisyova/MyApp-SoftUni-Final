a426740d356f29376b257c01d97178385ff0ee4b
Milena Kisyova <milena_kissyova@abv.bg>
2023-02-01 23:15:54 +0200

    Added IBooksService.

---
+++ BookstoreApp/Services/BookstoreApp.Services.Data/IBooksService.cs
@@ -0,0 +1,11 @@
+﻿namespace BookstoreApp.Services.Data
+{
+    using System.Collections.Generic;
+
+    using BookstoreApp.Web.ViewModels.Books;
+
+    public interface IBooksService
+    {
+        IEnumerable<AllBooksListViewModel> GetAll(int page, int itemsPerPage = 12);
+    }
+}
