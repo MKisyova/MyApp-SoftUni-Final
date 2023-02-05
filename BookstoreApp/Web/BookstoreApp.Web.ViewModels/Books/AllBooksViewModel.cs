db2634b6c022921c9498028264a00362889b7aa2
Milena Kisyova <milena_kissyova@abv.bg>
2023-02-05 17:18:17 +0200

    Made changes in AllBooksViewModel.

--- BookstoreApp/Web/BookstoreApp.Web.ViewModels/Books/AllBooksViewModel.cs
+++ BookstoreApp/Web/BookstoreApp.Web.ViewModels/Books/AllBooksViewModel.cs
@@ -1,6 +1,9 @@
 ﻿namespace BookstoreApp.Web.ViewModels.Books
 {
-    public class AllBooksViewModel
+    using BookstoreApp.Data.Models;
+    using BookstoreApp.Services.Mapping;
+
+    public class AllBooksViewModel : IMapFrom<Book>
     {
         public int Id { get; set; }
 
@@ -10,9 +13,16 @@
 
         public string Description { get; set; }
 
+        public int YearPublished { get; set; }
+
+        public int Pages { get; set; }
+
+        public decimal Price { get; set; }
+
         public int AuthorId { get; set; }
 
-        public string Author { get; set; }
+        public string AuthorFirstName { get; set; }
 
+        public string AuthorLastName { get; set; }
     }
 }
