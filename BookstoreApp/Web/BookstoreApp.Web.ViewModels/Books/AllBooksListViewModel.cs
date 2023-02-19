9c747caa30e8706ceacaae196ab67125d94b8459
Milena Kisyova <milena_kissyova@abv.bg>
2023-02-19 21:53:23 +0200

    Made changes in AllBooksListViewModel.cs

--- BookstoreApp/Web/BookstoreApp.Web.ViewModels/Books/AllBooksListViewModel.cs
+++ BookstoreApp/Web/BookstoreApp.Web.ViewModels/Books/AllBooksListViewModel.cs
@@ -3,24 +3,9 @@
     using System;
     using System.Collections.Generic;
 
-    public class AllBooksListViewModel
+    public class AllBooksListViewModel : PagingViewModel
     {
         public IEnumerable<AllBooksViewModel> Books { get; set; }
 
-        public int PageNumber { get; set; }
-
-        public bool HasPreviousPage => this.PageNumber > 1;
-
-        public int PreviousPageNumber => this.PageNumber - 1;
-
-        public bool HasNextPage => this.PageNumber < this.PagesCount;
-
-        public int NextPageNumber => this.PageNumber + 1;
-
-        public int PagesCount => (int)Math.Ceiling((double)this.BooksCount / this.ItemsPerPage);
-
-        public int BooksCount { get; set; }
-
-        public int ItemsPerPage { get; set; }
     }
 }
