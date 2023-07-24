363caa470e403014a8c2712c872a1c05d5e5bc20
Milena Kisyova <milena_kissyova@abv.bg>
2023-07-24 22:02:04 +0300

    Added GenresService

---
+++ BookstoreApp/Services/BookstoreApp.Services.Data/GenresService.cs
@@ -0,0 +1,41 @@
+﻿namespace BookstoreApp.Services.Data
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using BookstoreApp.Data.Common.Repositories;
+    using BookstoreApp.Data.Models;
+    using BookstoreApp.Web.ViewModels.Genres;
+
+    public class GenresService : IGenresService
+    {
+        private readonly IDeletableEntityRepository<Genre> genresRepository;
+
+        public GenresService(IDeletableEntityRepository<Genre> genresRepository)
+        {
+            this.genresRepository = genresRepository;
+        }
+
+        public async Task CreateAsync(CreateGenreInputModel input)
+        {
+            var genre = new Genre
+            {
+                Name = input.Name,
+            };
+
+            await this.genresRepository.AddAsync(genre);
+            await this.genresRepository.SaveChangesAsync();
+        }
+
+        public IEnumerable<KeyValuePair<string, string>> GetAllGenresAsKeyValuePair()
+        {
+            return this.genresRepository.AllAsNoTracking().Select(x => new
+            {
+                x.Id,
+                x.Name,
+            }).ToList().Select(x => new KeyValuePair<string, string>(x.Id.ToString(), x.Name));
+        }
+    }
+}
---
+++ BookstoreApp/Services/BookstoreApp.Services.Data/IGenresService.cs
@@ -0,0 +1,14 @@
+﻿namespace BookstoreApp.Services.Data
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    using BookstoreApp.Web.ViewModels.Genres;
+
+    public interface IGenresService
+    {
+        Task CreateAsync(CreateGenreInputModel input);
+
+        IEnumerable<KeyValuePair<string, string>> GetAllGenresAsKeyValuePair();
+    }
+}
