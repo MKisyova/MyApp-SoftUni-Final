6042c049fc3744e6c61a9475af2cdbf92cfbc92e
Milena Kisyova <milena_kissyova@abv.bg>
2023-07-27 22:48:54 +0300

    PagingViewModel fixes

--- BookstoreApp/Web/BookstoreApp.Web.ViewModels/PagingViewModel.cs
+++ BookstoreApp/Web/BookstoreApp.Web.ViewModels/PagingViewModel.cs
@@ -8,15 +8,15 @@
 
         public bool HasPreviousPage => this.PageNumber > 1;
 
-        public int PreviousPageNumber => this.PageNumber - 1;
-
         public bool HasNextPage => this.PageNumber < this.PagesCount;
+
+        public int PreviousPageNumber => this.PageNumber - 1;
 
         public int NextPageNumber => this.PageNumber + 1;
 
-        public int PagesCount => (int)Math.Ceiling((double)this.BooksCount / this.ItemsPerPage);
+        public int PagesCount => (int)Math.Ceiling((double)this.TotalItemsCount / this.ItemsPerPage);
 
-        public int BooksCount { get; set; }
+        public int TotalItemsCount { get; set; }
 
         public int ItemsPerPage { get; set; }
     }
