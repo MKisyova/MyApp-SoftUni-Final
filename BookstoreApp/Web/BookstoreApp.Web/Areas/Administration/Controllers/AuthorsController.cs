cf1c44a589e6eda0cc063d84739eb96ac7031010
Milena Kisyova <milena_kissyova@abv.bg>
2023-08-06 19:46:49 +0300

    Added AuthorsController and BooksController in area Administration with related Views

---
+++ BookstoreApp/Web/BookstoreApp.Web/Areas/Administration/Controllers/AuthorsController.cs
@@ -0,0 +1,61 @@
+﻿namespace BookstoreApp.Web.Areas.Administration.Controllers
+{
+    using System.Threading.Tasks;
+
+    using BookstoreApp.Common;
+    using BookstoreApp.Services.Data;
+    using BookstoreApp.Web.ViewModels.Administration.Authors;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class AuthorsController : AdministrationController
+    {
+        private readonly IAuthorsService authorsService;
+        private readonly IGenresService genresService;
+
+        public AuthorsController(
+            IAuthorsService authorsService,
+            IGenresService genresService)
+        {
+            this.authorsService = authorsService;
+            this.genresService = genresService;
+        }
+
+        public IActionResult All(int pageNumber = 1)
+        {
+            var viewModel = new AllAuthorsInTableViewModel
+            {
+                ActionName = nameof(this.All),
+                ItemsPerPage = GlobalConstants.ItemsPerPage,
+                PageNumber = pageNumber,
+                TotalItemsCount = this.authorsService.GetCount(),
+                Authors = this.authorsService
+                .GetAll<SingleAuthorInTableViewModel>(pageNumber, GlobalConstants.ItemsPerPage),
+            };
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            var model = new CreateAuthorInputModel();
+            model.Genres = this.genresService.GetAllGenresAsKeyValuePair();
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateAuthorInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                input.Genres = this.genresService.GetAllGenresAsKeyValuePair();
+                return this.View(input);
+            }
+
+            await this.authorsService.CreateAsync(input);
+
+            // redirect to all + tempData
+            return this.RedirectToAction(nameof(this.All));
+        }
+    }
+}
---
+++ BookstoreApp/Web/BookstoreApp.Web/Areas/Administration/Controllers/BooksController.cs
@@ -0,0 +1,81 @@
+﻿namespace BookstoreApp.Web.Areas.Administration.Controllers
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using BookstoreApp.Common;
+    using BookstoreApp.Services.Data;
+    using BookstoreApp.Web.ViewModels.Administration.Books;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Mvc;
+
+    public class BooksController : AdministrationController
+    {
+        private readonly IBooksService booksService;
+        private readonly IAuthorsService authorsService;
+        private readonly IGenresService genresService;
+        private readonly IWebHostEnvironment environment;
+
+        public BooksController(
+            IBooksService booksService,
+            IAuthorsService authorsService,
+            IGenresService genresService,
+            IWebHostEnvironment environment)
+        {
+            this.booksService = booksService;
+            this.authorsService = authorsService;
+            this.genresService = genresService;
+            this.environment = environment;
+        }
+
+        public IActionResult All(int pageNumber = 1)
+        {
+            var viewModel = new AllBooksInTableViewModel
+            {
+                ActionName = nameof(this.All),
+                ItemsPerPage = GlobalConstants.ItemsPerPage,
+                PageNumber = pageNumber,
+                TotalItemsCount = this.booksService.GetCount(),
+                Books = this.booksService
+                .GetAll<SingleBookInTableViewModel>(pageNumber, GlobalConstants.ItemsPerPage),
+            };
+
+            return this.View(viewModel);
+        }
+
+        public IActionResult Create()
+        {
+            var model = new CreateBookInputModel();
+            model.Authors = this.authorsService.GetAllAuthorsAsKeyValuePair();
+            model.Genres = this.genresService.GetAllGenresAsKeyValuePair();
+
+            return this.View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateBookInputModel input)
+        {
+            if (!this.ModelState.IsValid)
+            {
+                input.Authors = this.authorsService.GetAllAuthorsAsKeyValuePair();
+                input.Genres = this.genresService.GetAllGenresAsKeyValuePair();
+                return this.View(input);
+            }
+
+            try
+            {
+                await this.booksService.CreateAsync(input, $"{this.environment.WebRootPath}/images");
+            }
+            catch (Exception ex)
+            {
+                this.ModelState.AddModelError(string.Empty, ex.Message);
+                input.Authors = this.authorsService.GetAllAuthorsAsKeyValuePair();
+                input.Genres = this.genresService.GetAllGenresAsKeyValuePair();
+                return this.View(input);
+            }
+
+            // redirect to all + tempData
+            return this.RedirectToAction(nameof(this.All));
+        }
+    }
+}
