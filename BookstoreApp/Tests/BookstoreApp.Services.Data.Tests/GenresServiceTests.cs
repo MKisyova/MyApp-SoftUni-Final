c6e5e01d06f864cdd6829791f0de6916aa19e389
Milena Kisyova <milena_kissyova@abv.bg>
2023-08-31 17:14:32 +0300

    Added GenresServiceTests.cs

---
+++ BookstoreApp/Tests/BookstoreApp.Services.Data.Tests/GenresServiceTests.cs
@@ -0,0 +1,71 @@
+﻿namespace BookstoreApp.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using BookstoreApp.Data.Common.Repositories;
+    using BookstoreApp.Data.Models;
+    using BookstoreApp.Services.Mapping;
+    using BookstoreApp.Web.ViewModels;
+    using BookstoreApp.Web.ViewModels.Genres;
+    using Moq;
+    using Xunit;
+
+    public class GenresServiceTests
+    {
+        public GenresServiceTests()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+        }
+
+        [Fact]
+        public void GetCountShouldReturnTheTotalGenresCount()
+        {
+            GenresService service = this.MockService(this.TestData());
+
+            Assert.Equal(3, service.GetCount());
+        }
+
+        [Fact]
+        public void GetAllShouldReturnAllGenres()
+        {
+            GenresService service = this.MockService(this.TestData());
+
+            Assert.Equal(3, service.GetAll<SingleGenreViewModel>().Count());
+        }
+
+        private List<Genre> TestData()
+        {
+            return new List<Genre>
+            {
+                new Genre
+                {
+                    Name = "Action",
+                    IsFiction = true,
+                },
+                new Genre
+                {
+                    Name = "Horror",
+                    IsFiction = true,
+                },
+                new Genre
+                {
+                    Name = "Travel",
+                    IsFiction = false,
+                },
+            };
+        }
+
+        private GenresService MockService(List<Genre> genres)
+        {
+            var mockVotesRepo = new Mock<IDeletableEntityRepository<Genre>>();
+            mockVotesRepo.Setup(x => x.AllAsNoTracking()).Returns(genres.AsQueryable);
+            mockVotesRepo.Setup(x => x.AddAsync(It.IsAny<Genre>()))
+                .Callback((Genre genre) => genres.Add(genre));
+
+            var service = new GenresService(mockVotesRepo.Object);
+            return service;
+        }
+    }
+}
