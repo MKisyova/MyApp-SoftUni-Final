e50f26043954bd96080a521c7bf170af19d3c4ff
Milena Kisyova <milena_kissyova@abv.bg>
2023-09-01 11:08:25 +0300

    Added more tests in GenresServiceTests.

--- BookstoreApp/Tests/BookstoreApp.Services.Data.Tests/GenresServiceTests.cs
+++ BookstoreApp/Tests/BookstoreApp.Services.Data.Tests/GenresServiceTests.cs
@@ -8,6 +8,7 @@
     using BookstoreApp.Data.Models;
     using BookstoreApp.Services.Mapping;
     using BookstoreApp.Web.ViewModels;
+    using BookstoreApp.Web.ViewModels.Administration.Genres;
     using BookstoreApp.Web.ViewModels.Genres;
     using Moq;
     using Xunit;
@@ -24,7 +25,7 @@
         {
             GenresService service = this.MockService(this.TestData());
 
-            Assert.Equal(3, service.GetCount());
+            Assert.Equal(4, service.GetCount());
         }
 
         [Fact]
@@ -32,7 +33,64 @@
         {
             GenresService service = this.MockService(this.TestData());
 
-            Assert.Equal(3, service.GetAll<SingleGenreViewModel>().Count());
+            Assert.Equal(4, service.GetAll<SingleGenreViewModel>().Count());
+        }
+
+        [Fact]
+        public void GetAllFictionShouldReturnAllFictionGenres()
+        {
+            var mockVotesRepo = new Mock<IDeletableEntityRepository<Genre>>();
+            mockVotesRepo.Setup(x => x.AllAsNoTracking())
+                .Returns(this.TestData()
+                .Where(g => g.IsFiction).AsQueryable);
+
+            var service = new GenresService(mockVotesRepo.Object);
+
+            Assert.Equal(2, service.GetAllFiction<SingleGenreViewModel>().Count());
+        }
+
+        [Fact]
+        public void GetAllNonFictionShouldReturnAllNonFictionGenres()
+        {
+            var mockVotesRepo = new Mock<IDeletableEntityRepository<Genre>>();
+            mockVotesRepo.Setup(x => x.AllAsNoTracking())
+                .Returns(this.TestData()
+                .Where(g => g.IsFiction == false).AsQueryable);
+
+            var service = new GenresService(mockVotesRepo.Object);
+
+            Assert.Equal(2, service.GetAllNonfiction<SingleGenreViewModel>().Count());
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(1, 3, 3)]
+        [InlineData(1, 4, 4)]
+        public void GetAllWithPagingShouldReturnCorrectGenresCountPerPage(int page, int itemsPerPage, int expectedResult)
+        {
+            var mockVotesRepo = new Mock<IDeletableEntityRepository<Genre>>();
+            mockVotesRepo.Setup(x => x.AllAsNoTracking())
+                .Returns(this.TestData()
+                .Skip((page - 1) * itemsPerPage).Take(itemsPerPage).AsQueryable);
+
+            var service = new GenresService(mockVotesRepo.Object);
+
+            Assert.Equal(expectedResult, service.GetAllWithPaging<SingleGenreInTableViewModel>(page, itemsPerPage).Count());
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        public void GetByIdShouldReturnTheCorrectId(int id, int expectedResult)
+        {
+            var mockVotesRepo = new Mock<IDeletableEntityRepository<Genre>>();
+            mockVotesRepo.Setup(x => x.AllAsNoTracking())
+                .Returns(this.TestData()
+                .Where(g => g.Id == id).AsQueryable);
+
+            var service = new GenresService(mockVotesRepo.Object);
+            Assert.Equal(expectedResult, service.GetById<SingleGenreViewModel>(id).Id);
         }
 
         private List<Genre> TestData()
@@ -41,17 +99,26 @@
             {
                 new Genre
                 {
+                    Id = 1,
                     Name = "Action",
                     IsFiction = true,
                 },
                 new Genre
                 {
+                    Id = 2,
                     Name = "Horror",
                     IsFiction = true,
                 },
                 new Genre
                 {
+                    Id = 3,
                     Name = "Travel",
+                    IsFiction = false,
+                },
+                new Genre
+                {
+                    Id = 4,
+                    Name = "Philosophy",
                     IsFiction = false,
                 },
             };
@@ -61,9 +128,9 @@
         {
             var mockVotesRepo = new Mock<IDeletableEntityRepository<Genre>>();
             mockVotesRepo.Setup(x => x.AllAsNoTracking()).Returns(genres.AsQueryable);
-            mockVotesRepo.Setup(x => x.AddAsync(It.IsAny<Genre>()))
-                .Callback((Genre genre) => genres.Add(genre));
 
+            // mockVotesRepo.Setup(x => x.AddAsync(It.IsAny<Genre>()))
+            //    .Callback((Genre genre) => genres.Add(genre));
             var service = new GenresService(mockVotesRepo.Object);
             return service;
         }
