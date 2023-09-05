04c17c5b0ac84de7d8d773c4845d7646f9d56d2c
Milena Kisyova <milena_kissyova@abv.bg>
2023-09-05 21:43:43 +0300

    Added AuthorsServiceTests.

---
+++ BookstoreApp/Tests/BookstoreApp.Services.Data.Tests/AuthorsServiceTests.cs
@@ -0,0 +1,69 @@
+﻿namespace BookstoreApp.Services.Data.Tests
+{
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Reflection;
+
+    using BookstoreApp.Data.Common.Repositories;
+    using BookstoreApp.Data.Models;
+    using BookstoreApp.Services.Mapping;
+    using BookstoreApp.Web.ViewModels;
+    using Moq;
+    using Xunit;
+
+    public class AuthorsServiceTests
+    {
+        public AuthorsServiceTests()
+        {
+            AutoMapperConfig.RegisterMappings(typeof(ErrorViewModel).GetTypeInfo().Assembly);
+        }
+
+        [Fact]
+        public void GetCountShouldReturnTheTotalAuthorsCount()
+        {
+            AuthorsService service = this.MockService(this.TestData());
+
+            Assert.Equal(4, service.GetCount());
+        }
+
+        private List<Author> TestData()
+        {
+            return new List<Author>
+            {
+                new Author
+                {
+                    Id = 1,
+                    Name = "Ana",
+                },
+                new Author
+                {
+                    Id = 2,
+                    Name = "Maria",
+                },
+                new Author
+                {
+                    Id = 3,
+                    Name = "Ivan",
+                },
+                new Author
+                {
+                    Id = 4,
+                    Name = "Stoyan",
+                },
+            };
+        }
+
+        private AuthorsService MockService(List<Author> authors)
+        {
+            var mockAuthorsRepoAuthors = new Mock<IDeletableEntityRepository<Author>>();
+            mockAuthorsRepoAuthors.Setup(x => x.AllAsNoTracking()).Returns(authors.AsQueryable);
+            var mockAuthorsRepoGenres = new Mock<IDeletableEntityRepository<Genre>>();
+            var mockAuthorsRepoAuthorsGenres = new Mock<IRepository<AuthorGenre>>();
+
+            // mockVotesRepo.Setup(x => x.AddAsync(It.IsAny<Genre>()))
+            //    .Callback((Genre genre) => genres.Add(genre));
+            var service = new AuthorsService(mockAuthorsRepoAuthors.Object, mockAuthorsRepoGenres.Object, mockAuthorsRepoAuthorsGenres.Object);
+            return service;
+        }
+    }
+}
