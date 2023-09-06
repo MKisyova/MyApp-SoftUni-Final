01e62e1744f1ceb9db61ef47fb3f1a33c035134f
Milena Kisyova <milena_kissyova@abv.bg>
2023-09-06 21:04:45 +0300

    Added tests in AuthorsServiceTests.

--- BookstoreApp/Tests/BookstoreApp.Services.Data.Tests/AuthorsServiceTests.cs
+++ BookstoreApp/Tests/BookstoreApp.Services.Data.Tests/AuthorsServiceTests.cs
@@ -8,6 +8,7 @@
     using BookstoreApp.Data.Models;
     using BookstoreApp.Services.Mapping;
     using BookstoreApp.Web.ViewModels;
+    using BookstoreApp.Web.ViewModels.Authors;
     using Moq;
     using Xunit;
 
@@ -24,6 +25,59 @@
             AuthorsService service = this.MockService(this.TestData());
 
             Assert.Equal(4, service.GetCount());
+        }
+
+        [Theory]
+        [InlineData(1, 1)]
+        [InlineData(2, 2)]
+        [InlineData(3, 3)]
+        public void GetByIdShouldReturnTheCorrectId(int id, int expectedResult)
+        {
+            var mockRepoAuthors = new Mock<IDeletableEntityRepository<Author>>();
+            mockRepoAuthors.Setup(x => x.AllAsNoTracking())
+                .Returns(this.TestData()
+                .Where(g => g.Id == id).AsQueryable);
+            var mockRepoGenres = new Mock<IDeletableEntityRepository<Genre>>();
+            var mockRepoAuthorsGenres = new Mock<IRepository<AuthorGenre>>();
+
+            var service = new AuthorsService(
+               mockRepoAuthors.Object,
+               mockRepoGenres.Object,
+               mockRepoAuthorsGenres.Object);
+
+            Assert.Equal(expectedResult, service.GetById<SingleAuthorViewModel>(id).Id);
+        }
+
+        [Theory]
+        [InlineData(1, 2, 2)]
+        [InlineData(1, 3, 3)]
+        [InlineData(1, 4, 4)]
+        public void GetAllShouldReturnAllAuthors(int page, int itemsPerPage, int expectedResult)
+        {
+            var mockRepoAuthors = new Mock<IDeletableEntityRepository<Author>>();
+            mockRepoAuthors.Setup(x => x.AllAsNoTracking())
+                .Returns(this.TestData()
+                .Skip((page - 1) * itemsPerPage).Take(itemsPerPage).AsQueryable);
+            var mockRepoGenres = new Mock<IDeletableEntityRepository<Genre>>();
+            var mockRepoAuthorsGenres = new Mock<IRepository<AuthorGenre>>();
+
+            var service = new AuthorsService(
+               mockRepoAuthors.Object,
+               mockRepoGenres.Object,
+               mockRepoAuthorsGenres.Object);
+
+            Assert.Equal(expectedResult, service.GetAll<SingleAuthorViewModel>(page, itemsPerPage).Count());
+        }
+
+        [Fact]
+        public void GetAllAuthorsAsKeyValuePairShouldReturnAllAsKeyValuePair()
+        {
+            AuthorsService service = this.MockService(this.TestData());
+            var expectedResult = this.TestData()
+                .OrderBy(g => g.Name)
+                .Select(g => new KeyValuePair<string, string>(g.Id.ToString(), g.Name));
+
+            Assert.Equal(expectedResult, service.GetAllAuthorsAsKeyValuePair());
         }
 
         private List<Author> TestData()
@@ -55,14 +109,14 @@
 
         private AuthorsService MockService(List<Author> authors)
         {
-            var mockAuthorsRepoAuthors = new Mock<IDeletableEntityRepository<Author>>();
-            mockAuthorsRepoAuthors.Setup(x => x.AllAsNoTracking()).Returns(authors.AsQueryable);
-            var mockAuthorsRepoGenres = new Mock<IDeletableEntityRepository<Genre>>();
-            var mockAuthorsRepoAuthorsGenres = new Mock<IRepository<AuthorGenre>>();
+            var mockRepoAuthors = new Mock<IDeletableEntityRepository<Author>>();
+            mockRepoAuthors.Setup(x => x.AllAsNoTracking()).Returns(authors.AsQueryable);
+            var mockRepoGenres = new Mock<IDeletableEntityRepository<Genre>>();
+            var mockRepoAuthorsGenres = new Mock<IRepository<AuthorGenre>>();
 
             // mockVotesRepo.Setup(x => x.AddAsync(It.IsAny<Genre>()))
             //    .Callback((Genre genre) => genres.Add(genre));
-            var service = new AuthorsService(mockAuthorsRepoAuthors.Object, mockAuthorsRepoGenres.Object, mockAuthorsRepoAuthorsGenres.Object);
+            var service = new AuthorsService(mockRepoAuthors.Object, mockRepoGenres.Object, mockRepoAuthorsGenres.Object);
             return service;
         }
     }
